4eeecbfd4f7953ff029c7f5906a4b94df5c3452e
lee.yb <yboaee@beyondplatform.co.kr>
2019-07-25 18:54:37 +0900

    메인 페이지 1차

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.counter-value').each(function(){
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 5000,
+            easing: 'swing',
+            step: function (now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+})
