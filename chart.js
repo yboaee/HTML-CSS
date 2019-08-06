c611e0002188806ba118c66e0aaf0e78e6625bde
lee.yb <yboaee@beyondplatform.co.kr>
2019-08-06 18:35:28 +0900

    123123

--- chart.js
+++ chart.js
@@ -1,50 +1,123 @@
-var lineCtx = document.getElementById('lineChart');
-var lineChart = new Chart(lineCtx, {
-    type: 'line',
-    data: {
-        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        datasets: [{
-            label: 'Current week',
-            data: [1200000, 1900000, 300000, 500000, 200000, 300000, 1000000],
-            borderCapStyle: 'round',
-            borderColor: '#F13771',
-            borderJoinStyle: 'miter',
-            fill: 'Disabled',
-            pointHoverBackgroundColor: '#F13771',
-        },{
-            label: 'Previous week',
-            data: [1000000, 1500000, 500000, 800000, 300000, 100000, 400000],
-            borderCapStyle: 'round',
-            borderColor: '#BDBDBD',
-            borderJoinStyle: 'miter',
-            borderDash: [5, 8],
-            fill: 'Disabled',
-            pointHoverBackgroundColor: '#BDBDBD',
-        }]
-    },
-    options: {
-        legend:{
-            display: false
-        },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    display: false
-                }
-            }],
-            yAxes: [{
-                gridLines: {
-                    
-                },
-                ticks: {
-                    beginAtZero: true,
-                    lineHeight: '200%'
-                }
-            }]
-        }
-    }
-});
+window.onload=function(){
+    drawAmountInvested();
+    drawInvestorGrowthRateChart();
+    drawInvestmentGrowthRateChart();
+}
 
+/* 투자금액 */
+function drawAmountInvested(){
+    var lineCtx = document.getElementById('lineChart');
+    var lineChart = new Chart(lineCtx, {
+        type: 'line',
+        data: {
+            labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            datasets: [{
+                label: 'Current week',
+                data: [1200000, 1900000, 300000, 500000, 200000, 300000, 1000000],
+                borderCapStyle: 'round',
+                borderColor: '#F13771',
+                borderJoinStyle: 'miter',
+                fill: 'Disabled',
+                pointHoverBackgroundColor: '#F13771',
+            },{
+                label: 'Previous week',
+                data: [1000000, 1500000, 500000, 800000, 300000, 100000, 400000],
+                borderCapStyle: 'round',
+                borderColor: '#BDBDBD',
+                borderJoinStyle: 'miter',
+                borderDash: [5, 8],
+                fill: 'Disabled',
+                pointHoverBackgroundColor: '#BDBDBD',
+            }]
+        },
+        options: {
+            legend:{
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                yAxes: [{
+                    gridLines: {
+                        
+                    },
+                    ticks: {
+                        beginAtZero: true,
+                        lineHeight: '200%'
+                    }
+                }]
+            }
+        }
+    });
+}
+
+/* 투자자 증감률 */
+function drawInvestorGrowthRateChart(){
+
+    var doughnutCtx = document.getElementById('doughnutChart');
+    var doughnutChart = new Chart(doughnutCtx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Current', 'Previous'],
+            datasets:[{
+                        data: [92, 8],
+                        backgroundColor: ['#F13771', '#3BD98F']
+            }]
+        
+        },
+        options: {
+            legend:{
+                display: false
+            },
+            cutoutPercentage: 90,
+            elements: {
+                center: {
+                    text: '92%',
+                    color: '#F13771',
+                    fontStyle: 'Arial',
+                    sidePadding: 20
+                }
+            }
+        }
+    });
+}
+
+/* 투자액 증감률 */
+function drawInvestmentGrowthRateChart(){
+
+    var doughnut2Ctx = document.getElementById('doughnut2Chart');
+    var doughnut2Chart = new Chart(doughnut2Ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Current', 'Previous'],
+            datasets:[{
+                data: [64, 36],
+                backgroundColor: ['#FFFFFF', '#3BD98F']
+            }]
+            
+        },
+        options: {
+            legend:{
+                display: false
+            },
+            cutoutPercentage: 90,
+            elements: {
+                center: {
+                    text: '64%',
+                    color: '#FFFFFF',
+                    fontStyle: 'Arial',
+                    sidePadding: 20
+                }
+            }
+        }
+    });
+}
+
+
+/* Chart.js plugin */
 Chart.pluginService.register({
     beforeDraw: function (chart) {
       if (chart.config.options.elements.center) {
@@ -86,56 +159,3 @@ Chart.pluginService.register({
       }
     }
   });
-
-var doughnutCtx = document.getElementById('doughnutChart');
-var doughnutChart = new Chart(doughnutCtx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Current', 'Previous'],
-        datasets:[{
-            data: [92, 8],
-            backgroundColor: ['#F13771', '#3BD98F']
-        }]
-        
-    },
-    options: {
-        legend:{
-            display: false
-        },
-        cutoutPercentage: 90,
-        elements: {
-            center: {
-                text: '92%',
-                color: '#F13771',
-                fontStyle: 'Arial',
-                sidePadding: 20
-            }
-        }
-    }
-});
-var doughnut2Ctx = document.getElementById('doughnut2Chart');
-var doughnut2Chart = new Chart(doughnut2Ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Current', 'Previous'],
-        datasets:[{
-            data: [64, 36],
-            backgroundColor: ['#FFFFFF', '#3BD98F']
-        }]
-        
-    },
-    options: {
-        legend:{
-            display: false
-        },
-        cutoutPercentage: 90,
-        elements: {
-            center: {
-                text: '64%',
-                color: '#FFFFFF',
-                fontStyle: 'Arial',
-                sidePadding: 20
-            }
-        }
-    }
-});
