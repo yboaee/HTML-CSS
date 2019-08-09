5588573f16f001a97ce9dc3990610f751718445f
lee.yb <yboaee@beyondplatform.co.kr>
2019-08-09 18:33:12 +0900

    123

--- navbar.js
+++ navbar.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 /* SideBar Animation */
 function display_Sidebar(){
     $(".toggle_menu").click(function(){
-        $(".sidebar_menu").addClass("visible_menu");
+        $(".main-nav").addClass("visible_menu");
         $(".toggle_menu").addClass("opacity_zero");
 
         /* wrapper 같이 move */
@@ -16,11 +16,11 @@ function display_Sidebar(){
     });
 
     $(".fa-times").click(function(){
-        $(".sidebar_menu").removeClass("visible_menu");
+        $(".main-nav").removeClass("visible_menu");
         $(".toggle_menu").removeClass("opacity_zero");
 
         /* wrapper 같이 move */
-        $(".sidebar_menu").parents("div").css("width", "0px");
+        $(".main-nav").parents("div").css("width", "0px");
         $(".wrapper").css("margin-left", "0px");
         $(".wrapper").css("transition", "all 0.3s ease-in-out");
     });
